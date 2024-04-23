fa99fe65e9d766f80e9049872edda3adf4b86dcf
KeatonRasmussen <krasm002@odu.edu>
2024-04-22 21:37:17 -0400

    Finalized Median application with command-line functionality

--- src/cs252/TestMedian.java
+++ src/cs252/TestMedian.java
@@ -11,16 +11,23 @@ public class TestMedian {
             System.exit(1);
         }
         int n = Integer.parseInt(args[0]);
+        if (n > 6) {
+            System.err.println("Maximum of 6 tests are supported due to data constraints.");
+            System.exit(1);
+        }
         boolean result = test(n);
         if (result) {
             System.out.println("Passed all " + n + " tests.");
+        } else {
+            System.out.println("Failed some tests.");
         }
     }
-
 
     private static boolean test(int numTests) {
         double[] inputs = { 1.0, 4.0, 6.5, 2.4, 24.0, 5.0 };
         double[] expected = { 1.0, 2.5, 4.0, 3.2, 4.0, 4.5 };
+        
+        numTests = Math.min(numTests, inputs.length);
 
         for (int i = 0; i < numTests; ++i) {
             double[] trimmedInputs = Arrays.copyOf(inputs, i+1);
